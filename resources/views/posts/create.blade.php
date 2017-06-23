a261ff8079a3865908a5a7ca9046bcb931998af0
notahacker <mehmet@redloyalty.nl>
2017-06-23 13:35:34 +0200

    Laravel Collective added the Form options that are removed from the new Laravel, + made a input field where you can store text data.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,20 +7,34 @@
 <div class="row"> 
 <br><br><br><br>
 
-	<div class="col-md-8 col-md-offset-4">
+	<div class="col-md-8 col-md-offset-2">
 
 <h2> CREATE NEW POST </h2>
 <hr>
 <!-- Named routes && Adds CSRF Random tokens automaticly // Laravel Security -->
+<!--
 {!! Form::open(['route' => 'posts.store']) !!}
-    {{ Form::label('title') }}
-    {{ Form::text('title', null,array('class' => 'form-control'))}}
+    {{ Form::label('title', 'Title:') }}
+    {{ Form::text('title', null, array('class' => 'form-control')) }}
 
-{{ Form::label('body', "Post Body:")}}
+{{ Form::label('body', "Post Body:") }}
 {{ Form::textarea('body', null, array('class' => 'form-controller')) }}
 
-{{ Form::submit('Create Post')}}
+{{ Form::submit('Create Post', array('class' => 'btn btn-succes'))}}
 {!! Form::close() !!}
+--> 
+
+{!! Form::open(['route' => 'posts.store']) !!}
+    {{ Form::open(array('route' => 'posts.store')) }}
+    {{ Form::text('title', null, array('class' => 'form-control')) }}
+
+    {{ Form::label('body', "Post Body:") }}
+    {{ Form::textarea('body', null, array('class' => 'form-control')) }}
+
+    {{ Form::submit('Create Post')}}
+{!! Form::close() !!}
+
+
 
 
 	</div>
